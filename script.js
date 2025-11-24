0b524780105e1cd31e11ca999bf683df35ba28b9
Diego Marschinke <reapex2006@gmail.com>
2025-11-24 15:25:55 +0100

    Test

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function guessTheNumber() {
   } else if (guess.value < 0) {
     headline.innerHTML = " Die Zahl kann nicht kleiner als 0 sein.";
   } else if (numberToGuess > guess.value) {
-    headline.innerHTML = " Die Zahl ist größer!";
+    headline.innerHTML = " Die Zahl ist größer!!";
   } else if (numberToGuess < guess.value) {
     headline.innerHTML = " Die Zahl ist kleiner!";
   }
